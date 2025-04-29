79ac359cf394e90c8f87e7e01cc7621ce59c1f73
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-04-29 12:48:59 -0600

    ✨ dependencia de validation

--- src/main/java/com/boccia/api/model/User.java
+++ src/main/java/com/boccia/api/model/User.java
@@ -7,14 +7,19 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+
+@Entity
 @Getter
 @Setter
-@Entity
+@Table(name = "users")
 public class User{
 
+    @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private long id;
     private String name;
